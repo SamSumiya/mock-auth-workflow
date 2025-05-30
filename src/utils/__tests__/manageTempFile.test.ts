1bb8a37731fed663e93367d6f04357538a1e9380
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-05-30 14:25:08 -0700

    WIP: incomplete test for creation of a new dir and file

--- src/utils/__tests__/manageTempFile.test.ts
+++ src/utils/__tests__/manageTempFile.test.ts
@@ -1,5 +1,22 @@
-jest.mock('', () => {
+jest.mock('fs/promises', () => ({
+    mkdir: jest.fn(), 
+    writeFile: jest.fn()
+}))
 
-})
-
+import fs from 'fs/promises'
 import { createTempFile, cleanTempFiles } from "../manageTempFile"
+
+const mockMkdir = fs.mkdir as jest.Mock
+const mockWriteFile = fs.writeFile as jest.Mock
+
+describe('createTempFile', () => {
+    it('should create a new director and a file', async () => {
+        mockMkdir.mockResolvedValueOnce(undefined)
+        mockWriteFile.mockResolvedValueOnce(undefined)
+
+        await createTempFile('myFolder', 'test.json') 
+
+        expect(mockMkdir).toHaveBeenCalled()
+        expect(mockWriteFile).toHaveBeenCalled() 
+    })
+})
