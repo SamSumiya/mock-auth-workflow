48507d8870d339eee2772bb6001f818ff51b7415
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-05-30 15:28:37 -0700

    added a test with mocking UUID

--- src/utils/__tests__/manageTempFile.test.ts
+++ src/utils/__tests__/manageTempFile.test.ts
@@ -3,29 +3,63 @@ jest.mock('fs/promises', () => ({
     writeFile: jest.fn()
 }))
 
+jest.mock('crypto', () => ({
+    randomUUID: jest.fn() 
+}))
+
 import fs from 'fs/promises'
+import crypto from 'crypto'
 import { createTempFile, cleanTempFiles } from "../manageTempFile"
 
 const mockMkdir = fs.mkdir as jest.Mock
 const mockWriteFile = fs.writeFile as jest.Mock
+const mockRandomUUID = crypto.randomUUID as jest.Mock
+
 
 describe('createTempFile', () => {
-    it('should create a new director and a file', async () => {
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
+    it('should create a new director and a file with given content', async () => {
         mockMkdir.mockResolvedValueOnce(undefined)
         mockWriteFile.mockResolvedValueOnce(undefined)
 
-        const result = await createTempFile('myFolder', 'test.json') 
+        const result = await createTempFile('hello world', 'test.json') 
 
         expect(mockMkdir).toHaveBeenCalledWith(
             expect.stringContaining('__test__'),
             {recursive: true}
         )
+
         expect(mockWriteFile).toHaveBeenCalledWith(
             expect.stringContaining('test.json'),
-            'myFolder', 
+            'hello world', 
             'utf-8'
         ) 
 
         expect(result).toMatch(/__test__\/test\.json$/)
     })
+
+    it('should get a name from randomUUID when file name is not provided', async () => {
+        // Arrange 
+        mockMkdir(undefined)
+        mockWriteFile(undefined)
+        mockRandomUUID.mockReturnValueOnce('uniqname')
+
+        // Act 
+        const result = await createTempFile('hello world II') 
+
+        // Assert 
+        expect(mockMkdir).toHaveBeenCalledWith(
+            expect.stringContaining('__test__'), 
+            {recursive: true}
+        )
+        expect(mockWriteFile).toHaveBeenCalledWith(
+            expect.stringContaining('uniqname'), 
+            'hello world II', 
+            'utf-8'
+        )
+        expect(result).toMatch(/__test__\/uniqname\.json$/)
+    })  
 })
