8d36a078a5bcf0bca6d6276335ffde84b20f9b90
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-06-01 19:01:19 -0700

    updated the test cases again and add mockUUID

--- src/utils/__tests__/manageTempFile.test.ts
+++ src/utils/__tests__/manageTempFile.test.ts
@@ -11,132 +11,131 @@ import fs from 'fs/promises'
 import crypto from 'crypto'
 import { createTempFile, cleanTempFiles } from "../manageTempFile"
 
-const mockMkdir = fs.mkdir as jest.Mock
-const mockWriteFile = fs.writeFile as jest.Mock
-const mockRandomUUID = crypto.randomUUID as jest.Mock
+const mockMkdir = fs.mkdir as jest.MockedFunction<typeof fs.mkdir>;
+const mockWriteFile = fs.writeFile as jest.MockedFunction<typeof fs.writeFile>;
+const mockRandomUUID = crypto.randomUUID as jest.MockedFunction<typeof crypto.randomUUID>
 
-
-describe('createTempFile - success', () => {
+describe('createTempFile', () => {
     afterEach(() => {
         jest.clearAllMocks()
     }) 
 
-    it('should create a new directory and a file with given content', async () => {
-        // Arrange 
-        mockMkdir.mockResolvedValueOnce(undefined)
-        mockWriteFile.mockResolvedValueOnce(undefined)
+    describe('success cases ', () => {
+        it('should create a new directory and a file with given content', async () => {
+            // Arrange 
+            mockMkdir.mockResolvedValueOnce(undefined)
+            mockWriteFile.mockResolvedValueOnce(undefined)
 
-        // Act
-        const result = await createTempFile('hello world', 'test.json') 
+            // Act
+            const result = await createTempFile('hello world', 'test.json') 
 
-        // Assert
-        expect(mockMkdir).toHaveBeenCalledTimes(1)
-        expect(mockWriteFile).toHaveBeenCalledTimes(1)
+            // Assert
+            expect(mockMkdir).toHaveBeenCalledTimes(1)
+            expect(mockMkdir).toHaveBeenCalledWith(
+                expect.stringContaining('__test__'),
+                {recursive: true}
+            )
 
-        expect(mockMkdir).toHaveBeenCalledWith(
-            expect.stringContaining('__test__'),
-            {recursive: true}
-        )
+            expect(mockWriteFile).toHaveBeenCalledTimes(1)
+            expect(mockWriteFile).toHaveBeenCalledWith(
+                expect.stringContaining('test.json'),
+                'hello world', 
+                'utf-8'
+            ) 
 
-        expect(mockWriteFile).toHaveBeenCalledWith(
-            expect.stringContaining('test.json'),
-            'hello world', 
-            'utf-8'
-        ) 
+            expect(result).toMatch(/__test__\/test\.json$/)
+        })
 
-        expect(result).toMatch(/__test__\/test\.json$/)
+        it('should get a name from randomUUID when file name is not provided', async () => {
+            // Arrange 
+            const fakeID = '123e4567-e89b-12d3-a456-426614174000'
+            mockMkdir.mockResolvedValueOnce(undefined)
+            mockWriteFile.mockResolvedValueOnce(undefined)
+            // Watch out for mockReturn or mockResolved // 
+            mockRandomUUID.mockReturnValue(fakeID)
+
+            // Act 
+            const result = await createTempFile('hello world II') 
+
+            // Assert 
+            expect(mockMkdir).toHaveBeenCalledWith(
+                expect.stringContaining('__test__'), 
+                {recursive: true}
+            )
+
+            expect(mockWriteFile).toHaveBeenCalledWith(
+                expect.stringContaining(fakeID), 
+                'hello world II', 
+                'utf-8'
+            )
+
+            expect(result).toMatch(new RegExp(`__test__\\/${fakeID}\\.json$`)) 
+        })  
     })
 
-    it('should get a name from randomUUID when file name is not provided', async () => {
-        // Arrange 
-        mockMkdir.mockResolvedValueOnce(undefined)
-        mockWriteFile.mockResolvedValueOnce(undefined)
-        // Watch out for mockReturn or mockResolved // 
-        mockRandomUUID.mockReturnValueOnce('uniqname')
+    describe('error cases' , () => {
+        it('should reject when the file name already exists', async() => {
+            // Arrange
+            const eexistError = Object.assign(
+                new Error('EEXIST: Directory already exists'), 
+                { code: 'EEXIST'}
+            ) as NodeJS.ErrnoException
+            // less preferred  
+            // const eexistError = new Error('EEXIST: Directory already exists') as NodeJS.ErrnoException
+            // eexistError.code = 'EEXIST'
 
-        // Act 
-        const result = await createTempFile('hello world II') 
+            mockMkdir.mockRejectedValueOnce(eexistError)
 
-        // Assert 
-        expect(mockMkdir).toHaveBeenCalledWith(
-            expect.stringContaining('__test__'), 
-            {recursive: true}
-        )
+            // Act 
+            const promise = createTempFile('hello');
 
-        expect(mockWriteFile).toHaveBeenCalledWith(
-            expect.stringContaining('uniqname'), 
-            'hello world II', 
-            'utf-8'
-        )
+            // Asssert 
+            await expect(promise).rejects.toThrow('EEXIST')
+            await expect(promise).rejects.toMatchObject({ code: 'EEXIST'})
+            await expect(mockWriteFile).not.toHaveBeenCalled()
+        })
 
-        expect(result).toMatch(/__test__\/uniqname\.json$/)
-    })  
+        it ('should throw an ENOENT error when a parent directory does not exist', async () => {
+            // Arrange 
+            const enoentError = Object.assign(
+                new Error('ENOENT: No Dir nor no file'), 
+                {code: 'ENOENT'} 
+            ) as NodeJS.ErrnoException
 
-})
+            mockMkdir.mockRejectedValueOnce(enoentError)
 
-describe('createTempFile - fail' , () => {
-    afterEach(() => {
-        jest.clearAllMocks()
-    })
+            // Act
+            const promise = createTempFile('hello enoent')
 
-    it('should reject when the file name already exists', async() => {
-        // Arrange
-        const eexistError = Object.assign(
-            new Error('EEXIST: Directory already exists'), 
-            { code: 'EEXIST'}
-        ) as NodeJS.ErrnoException
-        // less preferred  
-        // const eexistError = new Error('EEXIST: Directory already exists') as NodeJS.ErrnoException
-        // eexistError.code = 'EEXIST'
+            // Assert
+            await expect(promise).rejects.toThrow('ENOENT: No Dir nor no file')
+            await expect(promise).rejects.toMatchObject({
+                message: expect.stringContaining('ENOENT'),
+                code: 'ENOENT'
+            });
+        })
 
-        mockMkdir.mockRejectedValueOnce(eexistError)
+        it ('should throw an EACCES error when there is a permission issue', async () => {
+            // Arrange 
+            const eaccesError = Object.assign(
+                new Error('EACCES: no permission'), 
+                { code: 'EACCES'} 
+            ) as NodeJS.ErrnoException 
 
-        // Act 
-        const promise = createTempFile('hello');
+            mockMkdir.mockRejectedValueOnce(eaccesError)
 
-        // Asssert 
-        await expect(promise).rejects.toThrow('EEXIST')
-        await expect(promise).rejects.toMatchObject({ code: 'EEXIST'})
-        await expect(mockWriteFile).not.toHaveBeenCalled()
-    })
+            // Act 
+            const promise = createTempFile('hello EACCES')
 
-    it ('should throw an ENOENT error when a parent directory does not exist', async () => {
-        // Arrange 
-        const enoentError = Object.assign(
-            new Error('ENOENT: No Dir nor no file'), 
-            {code: 'ENOENT'} 
-        ) as NodeJS.ErrnoException
-
-        mockMkdir.mockRejectedValueOnce(enoentError)
-
-        // Act
-        const promise = createTempFile('hello enoent')
-
-        // Assert
-        await expect(promise).rejects.toThrow('ENOENT: No Dir nor no file')
-        await expect(promise).rejects.toMatchObject({
-            message: expect.stringContaining('ENOENT'),
-            code: 'ENOENT'
-        });
-    })
-
-    it ('should throw an EACCES error when there is a permission issue', async () => {
-        // Arrange 
-        const eaccesError = Object.assign(
-            new Error('EACCES: no permission'), 
-            { code: 'EACCES'} 
-        ) as NodeJS.ErrnoException 
-
-        mockMkdir.mockRejectedValueOnce(eaccesError)
-
-        // Act 
-        const promise = createTempFile('hello EACCES')
-
-        // ASSERT
-        await expect(promise).rejects.toThrow('EACCES: no permission')
-        await expect(promise).rejects.toMatchObject({
-            message: expect.stringContaining('EACCES: no permission'), 
-            code: 'EACCES'
+            // ASSERT
+            await expect(promise).rejects.toThrow('EACCES: no permission')
+            await expect(promise).rejects.toMatchObject({
+                message: expect.stringContaining('EACCES: no permission'), 
+                code: 'EACCES'
+            })
         })
     })
 })
+
+
+
