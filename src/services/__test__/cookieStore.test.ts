e5681329b4a090b7fc769cd9a47e876f4253be13
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-06-04 16:55:56 -0700

    added my first success case for createCookie

--- src/services/__test__/cookieStore.test.ts
+++ src/services/__test__/cookieStore.test.ts
@@ -1,26 +1,19 @@
-jest.mock(('../cookieStore'), () => ({
-    setCookie: jest.fn()
-}))
+import { createCookie } from "../cookieStore"
 
-import { Cookie } from "../../types/cookie"
-import { cookieStore, setCookie } from "../cookieStore"
-import { persistCookie } from "../../repositories/cookieRepository"
 
-const mockSetCookie = setCookie as jest.MockedFunction<typeof setCookie>
+describe(('createCookie'), () => {
+    it('create a new cookie with guven value and expiresAt', () => {
+        // Arrange
+        const value = 'cookie'
+        const now = Date.now()
+        
+        // Act 
+        const result = createCookie(value, now)
 
-describe(('setCookie'), () => {
+        // Assert
+        expect(result.value).toBe('cookie')
+        expect(result.expiresAt).toBeGreaterThan(Date.now()) 
+    })
+
     
-    afterEach(() => {
-        jest.clearAllMocks()
-    })
-
-    it('writes cookie and calls persistCookie', async () => {
-        // Arrange && Act  
-        await setCookie('abc', 'sid', 'abc', 1000)
-
-        // Assert 
-        expect(cookieStore['abc']['sid'].value).toBe('abc') 
-        expect(cookieStore['abc']['sid'].expiresAt).toBeGreaterThan(Date.now())
-        expect(persistCookie).toHaveBeenCalled();
-    })
 })
